f597b3422a5468f0000df0045b93a672971f2cd8
Arturs Sosins <ar2rsawseen@gmail.com>
2018-01-31 00:49:46 +0200

    Update countly.install_travis.sh

--- bin/countly.install_travis.sh
+++ bin/countly.install_travis.sh
@@ -56,7 +56,8 @@ apt-get -y install supervisor || (echo "Failed to install supervisor." ; exit)
 #apt-get -y install sendmail
 
 #install grunt & npm modules
-( sudo su - -c "cd ${DIR}/.. ; npm install -g grunt-cli --unsafe-perm ; npm install --unsafe-perm" )
+npm --version
+( cd $DIR/.. ; sudo npm install -g grunt-cli --unsafe-perm ; sudo npm install --unsafe-perm )
 
 #install mongodb
 #bash $DIR/scripts/mongodb.install.sh
